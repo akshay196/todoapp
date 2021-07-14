d134673bdbaa58a8e823b57d3f804e8c3b437f04
Akshay Gaikwad <akgaikwad001@gmail.com>
2021-07-14 10:54:49 +0530

    Add Client Code

--- client.go
+++ client.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+var SERVERIP = "http://localhost:8000"
+
 func main() {
+	resp, err := http.Get(SERVERIP + "/list")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		fmt.Println("/list returned status not OK")
+		return
+	}
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response")
+		return
+	}
+	list := make(map[int]string)
+	err = json.Unmarshal(b, &list)
+	if err != nil {
+		fmt.Println("Error parsing the JSON data")
+		return
+	}
+	fmt.Println(list)
 }
